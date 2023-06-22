cff74d4b49df27547e994c22459ea7d5856e17ea
iamkipruto <kelvin.kipruto@student.moringaschool.com>
2023-06-22 11:42:31 +0300

    Tables Upgrade

--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, Column, String
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -13,6 +13,7 @@ class Zookeeper(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     birthday = db.Column(db.String)
+    environment = Column(String)
 
     animals = db.relationship('Animal', back_populates='zookeeper')
 
